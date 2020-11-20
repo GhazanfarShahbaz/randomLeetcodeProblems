42de1174ca71c299e416506fa64a961a18c35eee
GhazanfarShahbaz <ghazanfar.shahbaz2409@gmail.com>
2020-11-19 21:42:42 -0500

    Debig

--- bot.py
+++ bot.py
@@ -16,12 +16,13 @@ def createConnection():
 
 
 async def randomProblem(commands, message):
-    print(commands)
-    if len(commands) >= 2 and  not allowedDifficulties(commands[1]):
-        print("TEST")
-        return "You can only pick from these difficulties: Easy, Medium, Hard"
-    if  len(commands) == 3 and not allowedTags(commands[2]):
-        return "You can only pick from these tags: arrays, backtracking, binary_indexed_tree, binary_search, binary_search_tree, bit_manipulation, brain_teaser, breadth_first_search, depth_first_search, design, divide_and_conquer, dynamic_programming, geometry, graph, greedy, hash_table, heap, line_sweep, linked_lists, math, memoization, minimax, ordered_map, queue, random, recursion, rejection_sampling, reservoir_sampling, rolling_hash, segment_tree, sliding_window, sort, stack, string, suffix_array, topological_sort, tree, trie, two_pointers, union_find"
+    await message.channel.send(commands)
+    # print(commands)
+    # if len(commands) >= 2 and not allowedDifficulties(commands[1]):
+    #     print("TEST")
+    #     return "You can only pick from these difficulties: Easy, Medium, Hard"
+    # if  len(commands) == 3 and not allowedTags(commands[2]):
+    #     return "You can only pick from these tags: arrays, backtracking, binary_indexed_tree, binary_search, binary_search_tree, bit_manipulation, brain_teaser, breadth_first_search, depth_first_search, design, divide_and_conquer, dynamic_programming, geometry, graph, greedy, hash_table, heap, line_sweep, linked_lists, math, memoization, minimax, ordered_map, queue, random, recursion, rejection_sampling, reservoir_sampling, rolling_hash, segment_tree, sliding_window, sort, stack, string, suffix_array, topological_sort, tree, trie, two_pointers, union_find"
     
     tags = None if len(commands) < 3 else commands[2]
     difficulty = None if len(commands) < 2 else commands[1]
