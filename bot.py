83f0c80f6240f3a64c6ebb8988049f85ef746b81
GhazanfarShahbaz <ghazanfar.shahbaz2409@gmail.com>
2020-11-20 13:01:22 -0500

    Send messages in a code block

--- bot.py
+++ bot.py
@@ -21,16 +21,16 @@ async def helpUser(message, commands):
 
     if len(commands) == 2:
         if commands[1] in COMMANDS.keys():
-            await message.channel.send(f"Usage is as follows: {COMMANDS[commands[0]]['usage']}")
+            await message.channel.send(f"```Usage is as follows: {COMMANDS[commands[0]]['usage']}```")
             return
         else:
-            await message.channel.send("'''No such command exists'''")
+            await message.channel.send("```No such command exists```")
             return
     else:
         for command in COMMANDS.values():
             formString += f"{command['usage']} \n"
     
-    formString = f"'''{formString}'''"
+    formString = f"```{formString}```"
 
     await message.channel.send(formString)
 
@@ -39,10 +39,10 @@ async def randomProblem(message, commands):
     print("Random problem was called with the following commands", commands)
 
     if len(commands) >= 2 and not allowedDifficulties(commands[1]):
-        await message.channel.send("'''You can only pick from these difficulties: Easy, Medium, Hard'''")
+        await message.channel.send("```You can only pick from these difficulties: Easy, Medium, Hard```")
         return
     if  len(commands) == 3 and not allowedTags(commands[2]):
-        await message.channel.send("'''You can only pick from these tags: arrays, backtracking, binary_indexed_tree, binary_search, binary_search_tree, bit_manipulation, brain_teaser, breadth_first_search, depth_first_search, design, divide_and_conquer, dynamic_programming, geometry, graph, greedy, hash_table, heap, line_sweep, linked_lists, math, memoization, minimax, ordered_map, queue, random, recursion, rejection_sampling, reservoir_sampling, rolling_hash, segment_tree, sliding_window, sort, stack, string, suffix_array, topological_sort, tree, trie, two_pointers, union_find'''")
+        await message.channel.send("```You can only pick from these tags: arrays, backtracking, binary_indexed_tree, binary_search, binary_search_tree, bit_manipulation, brain_teaser, breadth_first_search, depth_first_search, design, divide_and_conquer, dynamic_programming, geometry, graph, greedy, hash_table, heap, line_sweep, linked_lists, math, memoization, minimax, ordered_map, queue, random, recursion, rejection_sampling, reservoir_sampling, rolling_hash, segment_tree, sliding_window, sort, stack, string, suffix_array, topological_sort, tree, trie, two_pointers, union_find```")
         return
     
     tag = None if len(commands) < 3 else commands[2]
@@ -61,7 +61,7 @@ async def randomProblem(message, commands):
         cursor.execute('select Count(*) as total from problems where difficulty = %s', (difficulty,))
         count = cursor.fetchall()[0][0]
         if count == 0:
-            await message.channel.send("'''Sorry no problems matched the criteria'''")
+            await message.channel.send("```Sorry no problems matched the criteria```")
             return
         randomNumber = randint(1, count)
         cursor.execute('select * from problems where number = %s', (randomNumber,))
@@ -72,14 +72,14 @@ async def randomProblem(message, commands):
         cursor.execute(script)
         count = cursor.fetchall()[0][0]
         if count == 0:
-            await message.channel.send("'''Sorry no problems matched the criteria'''")
+            await message.channel.send("```Sorry no problems matched the criteria```")
             return
         randomNumber = randint(1, count)
         cursor.execute('select * from problems where number = %s', (randomNumber,))
         link = cursor.fetchall()[0][3]
 
     connection.close()
-    await message.channel.send(f"'''{link}'''")
+    await message.channel.send(link)
 
 
 COMMANDS = {
@@ -116,12 +116,12 @@ async def on_message(message):
     if message.content.startswith("!question"):
         command = message.content.split()
         if command[1] not in COMMANDS:
-            await message.channel.send("'''Invalid command, use !questions help'''")
+            await message.channel.send("```Invalid command, use !questions help```")
         else:
             listLength = len(command) - 2
             if listLength < COMMANDS[command[1]]["required_params"] or listLength > COMMANDS[command[1]]['total_params']:
                 value = f'The parameters for this function are as follows {COMMANDS[command[1]]["usage"]} there are {COMMANDS[command[1]]["optional_params"]} optional paramters'
-                await message.channel.send(f"'''{value}'''")
+                await message.channel.send(f"```{value}```")
             else:
                 await COMMANDS[command[1]]['function'](message, command[1:])
 
@@ -133,7 +133,7 @@ async def dailyQuestion():
     cursor.execute('SELECT * from problems WHERE number = %s', (randomNumber,))
     link = cursor.fetchall()[0][3]
     connection.close()
-    await client.get_channel(758441730701131805).send(f"'''Daily Question: \n {link}'''")
+    await client.get_channel(758441730701131805).send(f"```Daily Question: \n {link}```")
 
 
 client.run(os.environ["TOKEN"])
