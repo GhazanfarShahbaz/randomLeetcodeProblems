5216ccf0011d2fb63b28a04c82791e623128acb1
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-06-02 21:01:34 +0000

    document message dict

--- utility/messageDict.py
+++ utility/messageDict.py
@@ -1,4 +1,6 @@
-messageSyntax = {
+from typing import Dict, Optional
+
+messageSyntax: Dict[str, str] = {
     "c": "c",
     "c++": "cpp",
     "c#": "c",
@@ -17,8 +19,26 @@ messageSyntax = {
 }
 
 
-def getLanguageCode(lang : str) -> None or str:
+def getLanguageCode(lang: str) -> Optional[str]:
+    """
+    Returns the language code that corresponds to the given language, or None if the language is not recognized.
+
+    Args:
+        lang (str): The name of the language.
+
+    Returns:
+        Optional[str]: The language code that corresponds to the given language, or None if the language is not recognized.
+    """
     return None if lang.lower() not in messageSyntax.keys() else messageSyntax[lang.lower()]
 
-def checkLanguage(lang : str) -> str:
+def checkLanguage(lang: str) -> bool:
+    """
+    Returns whether the given language is currently supported by the bot.
+
+    Args:
+        lang (str): The language to check.
+
+    Returns:
+        bool: True if the language is supported, False otherwise.
+    """
     return lang.lower() in messageSyntax.keys()
